b1f5c415d31159c66740c697320a641deba39b4e
T-107 <T-107@PC246>
2015-09-21 19:40:16 -0500

    quibolobo17/sep

--- JavaApplication1/src/javaapplication1/Imc.java
+++ JavaApplication1/src/javaapplication1/Imc.java
@@ -18,8 +18,26 @@ public class Imc {
           altura=1.68f;
       }
     
-    
-    public float calcular() {
+      public Imc(float f){
+          //este codigo no tiene que ver con los con los contructores
+          byte  b=2;
+          short s=2;
+                  int i=2;
+                  long l=2;
+                 // reasignamos 
+  /*casting forzado */     
+                  b=(byte)i;
+                  l=i;
+                  s=(short)l;
+                  s=b;
+                  l=s;
+                  s=(short)l;
+                  s=(short)i;
+                  l=b;
+                  
+      }
+      
+        public float calcular() {
         
         float resultado=peso/(altura*altura);
         return resultado;
